438ea9856cc1941761bca174a155b1c1afe7aee4
shiyu99 <53148393+shiyu99@users.noreply.github.com>
2021-06-16 17:46:14 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -5,7 +5,7 @@ const repoURL = "https://github.com/shiyu99/shiyu99.github.io.git";
 ghpages.publish(
   pathname,
   {
-    branch: "master",
+    branch: "main",
     repo: repoURL,
   },
   (err) => {
