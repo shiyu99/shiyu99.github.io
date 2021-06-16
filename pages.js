6ef5b0b417354c7101f87d66f4387c72df352c35
shiyu99 <53148393+shiyu99@users.noreply.github.com>
2021-06-16 00:56:42 -0400

    incorporating open-source template

---
+++ pages.js
@@ -0,0 +1,15 @@
+const ghpages = require("gh-pages");
+const pathname = `${__dirname}/build`;
+const repoURL = "https://github.com/shiyu99/shiyu99.github.io.git";
+
+ghpages.publish(
+  pathname,
+  {
+    branch: "master",
+    repo: repoURL,
+  },
+  (err) => {
+    if (err) console.log("ERROR: ", err);
+    else console.log("PUBLISHED");
+  }
+);
