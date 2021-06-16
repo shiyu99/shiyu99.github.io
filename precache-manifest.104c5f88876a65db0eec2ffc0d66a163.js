f79f4aaa8fb209582eac02f1697b69c7feaa35af
shiyu99 <53148393+shiyu99@users.noreply.github.com>
2021-06-16 00:33:06 -0400

    Updates

---
+++ precache-manifest.104c5f88876a65db0eec2ffc0d66a163.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcf3b9bac7b7a20797ac71b542e0d6c",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "128cc95a2217cde27c74",
+    "url": "/home/static/css/main.9d1d1a0e.chunk.css"
+  },
+  {
+    "revision": "c6ca6c84ee2a02bfc5f5",
+    "url": "/home/static/js/2.75376d99.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.75376d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128cc95a2217cde27c74",
+    "url": "/home/static/js/main.3f7cef04.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
